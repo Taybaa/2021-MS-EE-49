f6155a15728ea7f00a508aee01a0ae3fb2ffbf48
Tayyaba <87190676+Taybaa@users.noreply.github.com>
2022-04-15 23:46:19 +0500

    Update Generator.sv

--- sim/Generator.sv
+++ sim/Generator.sv
@@ -1,115 +1,102 @@
 `include "Transaction1.sv"
+class burst;
+rand 	logic 	[7:0] 	HADDR;
+rand logic 		HWRITE; 
+rand logic	[2:0]  	HSIZE;
+rand logic	[2:0] 	HBURST;
+rand logic 	[3:0] 	HPROT;
+rand logic	[1:0]	HTRANS;
+rand	logic 	[31:0] 	HWDATA;
+logic 		HREADYOUT;
+logic 		HRESP;
+logic 	[31:0] 	HRDATA;
+	
+task create(input [2:0] burst_type, input [2:0] burst_size, int wrap_or_incr_size);
+int wrap = 0;
+int addr;
+addr = HADDR;
+if(burst_type == 3'b010 || 3'b100 || 3'b110)
+begin
+	for(int i = 0;i < 40;i++)
+		begin
+		if( wrap <wrap_or_incr_size) 
+		begin
+		HADDR 	<= HADDR + 1;
+		wrap++;
+		end
+		else 
+		begin
+		HADDR 	<= addr;
+		wrap 	 = 0;
+		end
+		HSIZE 	<= burst_size;			
+		HBURST 	<= burst_type;
+		if(i==0)
+			HTRANS <= 2'b10;
+		else 	HTRANS <= 2'b11;
+		HPROT 	<= 4'b0011;
+	end
+end
+else
+begin
+	for(int i = 0;i <  wrap_or_incr_size;i++)
+	begin
+		if(i> 0) 
+			HADDR 	<= HADDR + 1;
+		if(i==0)
+			HTRANS 	<= 2'b10;
+		else 	
+			HTRANS 	<= 2'b11;
+		HPROT	<= 4'b0011;
+		HSIZE 	<= burst_size;			
+		HBURST 	<= burst_type;
+	end
+end
+	
+
+endtask 
+endclass
+
+//.....Generator Class.....//
+
 class generator;
-   
-  //declaring transaction class
- rand transaction trans;
-   
+ rand burst bst;
   //declaring mailbox
   mailbox gen2driv;
-
-logic 	[31:0] 	haddr;
-logic 		hwrite;
-logic [31:0]hwdata;
-int b; //beats
-logic [2:0] hsize;
-
+	
+//.....Size......//
+localparam WORD		= 010;
+localparam H_WORD	= 001;
+localparam BYTE		= 000;
+	
  function new(mailbox gen2driv);
     this.gen2driv <= gen2driv;    
   endfunction
 
-localparam WORD		= 010;
-
-task SB();	//Single Burst	
-		trans = new; 
-		this.trans.HADDR=haddr;
-		this.trans.HWDATA=hwdata;
-		this.trans.HBURST = 3'b000; 
-       	 	this.trans.HWRITE = hwrite; 
-     		this.trans.HTRANS = 3'b010; // Non-Sequential 
-		gen2driv.put(trans);
-		$display("Address:0x%0h towards driver\n",trans.HADDR);
-	 		b=b-1;
-		repeat(b)	
-      			begin
-			trans=new;
-       			this.trans.HWRITE = hwrite; 
-       			this.trans.HBURST = 3'b000; 
-      			this.trans.HTRANS = 3'b011; // SEQ for the remaining beats
-      			this.trans.HSIZE = WORD; // (32-bits)
-      			case(trans.HSIZE)
-				3'b000 : hsize = 1;
-              			3'b001 : hsize = 2;
-          			3'b010 : hsize = 4;
-        		endcase 
-       			gen2driv.put(trans);	
-			haddr=haddr+hsize;
-			this.trans.HADDR = haddr;
-			$display("Address:0x%0h towards mailbox",trans.HADDR);
-			end
+task single_burst();
+bst=new;
+bst.randomize();
+bst.create(3'b001,BYTE,100);
+gen2drive.put(bst);
+endtask	
 	
-		$display("Single burst sent to driver\n");
-		endtask  
+task wrap4_burst();
+bst=new;
+bst.randomize();
+bst.create(3'b010,BYTE,4);
+gen2drive.put(bst);
+endtask	
 
- task INCR4();			
-		trans = new; 
-		this.trans.HADDR=haddr;
-		this.trans.HWDATA=hwdata;
-		this.trans.HBURST = 3'b011; //4 Beat Incrementing Burst
-       	 	this.trans.HWRITE = hwrite; 
-     		this.trans.HTRANS = 3'b010; // Non-Sequential 
-		gen2driv.put(trans);
-		$display("Address:0x%0h towards driver\n",trans.HADDR);
-	 		
-		repeat(3)	
-      			begin
-			trans=new;
-       			this.trans.HWRITE = hwrite; 
-       			this.trans.HBURST = 3'b011; //4 Beat Incrementing Burst
-      			this.trans.HTRANS = 3'b011; // SEQ for the remaining beats
-      			this.trans.HSIZE = WORD; // (32-bits)
-      			case(trans.HSIZE)
-				3'b000 : hsize = 1;
-              			3'b001 : hsize = 2;
-          			3'b010 : hsize = 4;
-        		endcase 
-       			gen2driv.put(trans);	
-			haddr=haddr+hsize;
-			this.trans.HADDR = haddr;
-			$display("Address:0x%0h towards mailbox",trans.HADDR);
-			end
+task incr4_burst();
+bst=new;
+bst.randomize();
+bst.create(3'b011,BYTE,4);
+gen2drive.put(bst);
+endtask	
 	
-		$display("INCR4 burst sent to driver\n");
-		endtask  
-
-task INCR8();	
-                trans = new; 
-		this.trans.HADDR=haddr;
-		this.trans.HWDATA=hwdata;
-		this.trans.HBURST = 3'b101; //8 Beat Incrementing Burst
-       	 	this.trans.HWRITE = hwrite; 
-     		this.trans.HTRANS = 3'b010; // Non-Sequential 
-		gen2driv.put(trans);
-		$display("Address:0x%0h towards driver\n",trans.HADDR);
-	 		
-		repeat(7)	
-      			begin
-			trans=new;
-       			this.trans.HWRITE = hwrite; 
-       			this.trans.HBURST = 3'b101; //8 Beat Incrementing Burst
-      			this.trans.HTRANS = 3'b011; // SEQ for the remaining beats
-      			this.trans.HSIZE = WORD; // (32-bits)
-      			case(trans.HSIZE)
-				3'b000 : hsize = 1;
-              			3'b001 : hsize = 2;
-          			3'b010 : hsize = 4;
-        		endcase 
-       			gen2driv.put(trans);	
-			haddr=haddr+hsize;
-			this.trans.HADDR = haddr;
-			$display("Address:0x%0h towards mailbox",trans.HADDR);
-			end
-	
-		$display("INCR8 burst sent to driver\n");
-		endtask  
-  
+task run;
+single_burst();
+incr4_burst();
+wrap4_burst();
+endtask
 endclass
