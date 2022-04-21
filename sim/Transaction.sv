86bf659d6ab4ca3cb1374b6547d15738a2ca20e0
Tayyaba <87190676+Taybaa@users.noreply.github.com>
2022-04-21 23:45:17 +0500

    Update Transaction.sv

--- sim/Transaction.sv
+++ sim/Transaction.sv
@@ -1,13 +1,16 @@
 class burst;
 rand 	logic 	[7:0] 	HADDR;
-rand logic 		HWRITE; 
-rand logic	[2:0]  	HSIZE;
-rand logic	[2:0] 	HBURST;
-rand logic 	[3:0] 	HPROT;
-rand logic	[1:0]	HTRANS;
+logic 		HWRITE; 
+logic	[2:0]  	HSIZE;
+logic	[2:0] 	HBURST;
+logic 	[3:0] 	HPROT;
+logic	[1:0]	HTRANS;
 rand	logic 	[31:0] 	HWDATA;
 logic 		HREADYOUT;
 logic 		HRESP;
 logic 	[31:0] 	HRDATA;
+bit HRESET;
+bit HSEL;
 
+ 
 endclass
