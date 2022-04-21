48fbbaa1af14c045608f30b6e027d44089f350b4
Tayyaba <87190676+Taybaa@users.noreply.github.com>
2022-04-21 23:48:44 +0500

    Update Wrapper.sv

--- sim/Wrapper.sv
+++ sim/Wrapper.sv
@@ -6,25 +6,28 @@ interface wrap (input bit HCLK,HRESET);
    logic [2:0] HSIZE;
    logic [2:0] HBURST;
    logic [1:0] HTRANS, HSEL;
-   logic HREADY, HREADYOUT, HRESP, HWRITE;
+   logic HREADY, HREADYOUT, HRESP, HWRITE, HMASTLOCK;
 endinterface : wrap
 
-module wrapper (port_w, port_if);
-    wrap port_w;
-    AHB port_if;
-    assign port_w.HADDR = port_if.HADDR;
-    assign port_w.HRDATA = port_if.HRDATA;
-    assign port_w.HWDATA = port_if.HWDATA;
-    assign port_w.HPROT = port_if.HPROT;
-    assign port_w.HSIZE = port_if.HSIZE;
-    assign port_w.HBURST = port_if.HBURST;
-    assign port_w.HTRANS = port_if.HTRANS;
-    assign port_w.HSEL = port_if.HSEL;
-    assign port_w.HREADY = port_if.HREADY;
-    assign port_w.HREADYOUT = port_if.HREADYOUT;
-    assign port_w.HRESP = port_if.HRESP;
-    assign port_w.HWRITE = port_if.HWRITE;
-    assign port_w.HCLK = port_if.HCLK;
-    assign port_w.HRESET = port_if.HRESET;
+module wrapper (port_a, port_w);
+    ahb_intf port_a;
+wrap port_w;
+    
+    assign port_w.HADDR = port_a.HADDR;
+    assign port_w.HRDATA = port_a.HRDATA;
+    assign port_w.HWDATA = port_a.HWDATA;
+    assign port_w.HPROT = port_a.HPROT;
+    assign port_w.HSIZE = port_a.HSIZE;
+    assign port_w.HBURST = port_a.HBURST;
+    assign port_w.HTRANS = port_a.HTRANS;
+    assign port_w.HSEL = port_a.HSEL;
+    assign port_w.HREADY = port_a.HREADY;
+    assign port_w.HREADYOUT = port_a.HREADYOUT;
+    assign port_w.HRESP = port_a.HRESP;
+    assign port_w.HWRITE = port_a.HWRITE;
+    assign port_w.HCLK = port_a.HCLK;
+    assign port_w.HRESET = port_a.HRESET;
+assign port_w.HMASTLOCK = port_a.HMASTLOCK;
+
 endmodule: wrapper
 
