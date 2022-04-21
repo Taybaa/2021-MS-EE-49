c23c7f93b199101cd3fbb7eb45f1250a7f7cb217
Tayyaba <87190676+Taybaa@users.noreply.github.com>
2022-04-22 01:04:08 +0500

    Update Test.sv

--- sim/Test.sv
+++ sim/Test.sv
@@ -1,13 +1,39 @@
 `include "Enviornment.sv"
 program test(ahb_intf intf);
-   
+   environment env;
+  localparam bursts = 1000;		// In order to write or read random many bursts
+  logic [2:0] br_type;
+  logic [2:0] br_size;
+  int incrwrap_size; 
  
    
   initial begin
- environment env;
-    
- env.run();
-   
+
+   env = new(intf); 
+test_9();
+	$stop;
   end
+
+
+task randomize();
+forever begin
+	br_type = $random;
+        br_size = $random;
+        incrwrap_size = $random;
+
+	env.gen.create_burst(br_type,br_size,incrwrap_size);
+	wait(env.gen.repeat_count == bursts);
+	$stop;
+end
+endtask
+
+task test_9();
+gen.addr = 'h3C ;
+env.gen.wrap4_burst();
+
+env.run();
+
+endtask
+
 endprogram
 
